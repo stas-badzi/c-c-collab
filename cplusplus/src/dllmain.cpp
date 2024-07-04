efb51db9318e0bb84e70ff6f8f0af74665958d62
stas-badzi <sss.badziak@gmail.com>
2024-07-04 23:49:20 +0200

    finished exportig console class functions c++

--- cplusplus/src/dllmain.cpp
+++ cplusplus/src/dllmain.cpp
@@ -3,26 +3,40 @@
 // Console
     #include "Console.hpp"
 
-    libexport std::array<unsigned long,2> Console_FillConsole(cpp::Console::Symbol** symbols, int width, int height) {
-        cpp::Console::FillConsole(symbols);
+    libexport int* Console_FillConsole$ret2(cpp::Console::Symbol** symbols, int width, int height) {
+        std::vector<std::vector<cpp::Console::Symbol>> vecsyms;
+        for (int i = 0; i < height; i++) {
+            std::vector<cpp::Console::Symbol> vecdsym;
+            for (int j = 0; j < width; j++) {
+                vecdsym.push_back(symbols[i][j]);
+            }
+            vecsyms.push_back(vecdsym);
+        }
+        unsigned int out[2];
+        std::array<unsigned long int,2> ret = cpp::Console::FillConsole(vecsyms);
+        out[0] = ret[0];
+        out[1] = ret[1];
+        return (int*)out;
     }
 
     // Symbol
         libexport void* Console_Symbol_Constuct(wchar_t character = L' ', char foreground = 7, char background = 0) {
             return (void*) new cpp::Console::Symbol(character, foreground, background);
         }
+        
+        #ifdef _WIN32
+            libexport void* Console_Symbol_Constuct$atr(char attribute) {
+                return (void*) new cpp::Console::Symbol(attribute);
+            }
+            
+            libexport void Console_Symbol_SetAttribute(cpp::Console::Symbol* smb, char attribute) {
+                smb->SetAttribute(attribute);
+            }
 
-        libexport void* Console_Symbol_Constuct$atr(char attribute) {
-            return (void*) new cpp::Console::Symbol(attribute);
-        }
-
-        libexport void Console_Symbol_SetAttribute(cpp::Console::Symbol* smb, char attribute) {
-            smb->SetAttribute(attribute);
-        }
-
-        libexport char Console_Symbol_GetAttribute(cpp::Console::Symbol* smb) {
-            return smb->GetAttribute();
-        }
+            libexport char Console_Symbol_GetAttribute(cpp::Console::Symbol* smb) {
+                return smb->GetAttribute();
+            }
+        #endif
 
         libexport void Console_Symbol_character$set(cpp::Console::Symbol* smb, wchar_t character) {
             smb->character = character;
