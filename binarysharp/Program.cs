3a18b9740192469ac43115047fe60442868ff711
majkol <michal.kindziuk@gmail.com>
2024-07-05 20:07:16 +0200

    made a FileExport function

--- binarysharp/Program.cs
+++ binarysharp/Program.cs
@@ -8,6 +8,9 @@ namespace CsImp {
         //import
         [DllImport(DllSource, CallingConvention = CallingConvention.Cdecl, EntryPoint = "FileSystem_ImportText")]
         public static extern IntPtr ImportText(IntPtr fileptr);
+        //import
+        [DllImport(DllSource, CallingConvention = CallingConvention.Cdecl, EntryPoint = "FileSystem_ExportText")]
+        public static extern void ExportText(IntPtr pathptr, IntPtr contentptr);
     }
 }
 
@@ -15,6 +18,9 @@ namespace Cs {
     public class FileSystem {
         public static string ImportText(string filestr) {
             return Marshal.PtrToStringAuto(CsImp.FileSystem.ImportText(Marshal.StringToHGlobalUni(filestr)));
+        }
+        public static void ExportText(string pathstr, string contentstr) {
+            CsImp.FileSystem.ExportText(Marshal.StringToHGlobalUni(pathstr), Marshal.StringToHGlobalUni(contentstr));
         }
     }
 
@@ -24,6 +30,7 @@ namespace Cs {
             string filename = Directory.GetCurrentDirectory() + @"/../../../csbin";
             Console.WriteLine("Acessing file: " + filename);
             Console.WriteLine(FileSystem.ImportText(filename));
+            FileSystem.ExportText((Directory.GetCurrentDirectory() + @"/../../../csharp"), "Hejka");
         }
 
     }
