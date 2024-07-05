49e57697343e08ea463b268ffb523a5745b671fe
stas-badzi <151390437+stas-badzi@users.noreply.github.com>
2024-07-05 16:34:05 +0200

    Make C# Program functions easier to access

--- binarysharp/Program.cs
+++ binarysharp/Program.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace CppCsComTest {
-    public class FlImp {
+namespace CsImp {
+    public class FileImport {
         private const string DllSource =  @"" + DllHandle.Prefix + "csharp" + DllHandle.Suffix;
         //import
         [DllImport(DllSource, CallingConvention = CallingConvention.Cdecl, EntryPoint = "FileImport_ImportTxt")]
-        public static extern IntPtr Imptxt(IntPtr file);
+        public static extern IntPtr ImportText(IntPtr file);
     }
+}
+
+namespace Cs {
+    public class FileImport {
+        public static string ImportText(string file) {
+            return Marshal.PtrToStringAuto(CsImp.FileImport.ImportText(Marshal.StringToHGlobalUni(file)))
+        }
+    }
+
     public class Program {
 
         static void Main(string[] args) {
-            string file = "C:/Users/micha/Desktop/mswindows_script.txt";
-            IntPtr strptr = Marshal.StringToHGlobalUni(file);
-            Console.WriteLine(Marshal.PtrToStringAuto(FlImp.Imptxt(strptr)));
+            string filename = "C:/Users/micha/Desktop/mswindows_script.txt";
+            Console.WriteLine(FileImport.ImportText(filename));
         }
 
     }
