af7448c26793d2cebcaefebaa368a5d595165afb
stas-badzi <sss.badziak@gmail.com>
2024-07-05 19:04:51 +0200

    Renamed C# FileImport Class to FileSystem

--- binarysharp/Program.cs
+++ binarysharp/Program.cs
@@ -3,18 +3,18 @@ using System.Runtime;
 using System.Runtime.InteropServices;
 
 namespace CsImp {
-    public class FileImport {
+    public class FileSystem {
         private const string DllSource =  @"" + DllHandle.Prefix + "csharp" + DllHandle.Suffix;
         //import
         [DllImport(DllSource, CallingConvention = CallingConvention.Cdecl, EntryPoint = "FileImport_ImportTxt")]
-        public static extern IntPtr ImportText(IntPtr file);
+        public static extern IntPtr ImportText(IntPtr fileptr);
     }
 }
 
 namespace Cs {
-    public class FileImport {
-        public static string ImportText(string file) {
-            return Marshal.PtrToStringAuto(CsImp.FileImport.ImportText(Marshal.StringToHGlobalUni(file)));
+    public class FileSystem {
+        public static string ImportText(string filestr) {
+            return Marshal.PtrToStringAuto(CsImp.FileImport.ImportText(Marshal.StringToHGlobalUni(filestr)));
         }
     }
 
@@ -23,7 +23,7 @@ namespace Cs {
         static void Main(string[] args) {
             string filename = Directory.GetCurrentDirectory() + @"/../../../csbin";
             Console.WriteLine("Acessing file: " + filename);
-            Console.WriteLine(FileImport.ImportText(filename));
+            Console.WriteLine(FileSystem.ImportText(filename));
         }
 
     }
