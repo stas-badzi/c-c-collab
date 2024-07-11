c8904c38361871f95138fb07661c6bd15f6b8695
stas-badzi <151390437+stas-badzi@users.noreply.github.com>
2024-07-11 13:39:31 +0200

    Update DllImport.cs

--- binarysharp/DllImport.cs
+++ binarysharp/DllImport.cs
@@ -46,8 +46,45 @@ namespace CsImp {
         [DllImport(DllSource, CallingConvention = CallingConvention.Cdecl, EntryPoint = "FileSystem_ExportText")]
         public static extern void ExportText(IntPtr pathptr, IntPtr contentptr);
     }
+}
 
-    public class ConsoleInterop
+namespace CppImp {
+
+    public class DllHandle {
+
+    #if _WIN32
+        public const string Prefix = "";
+        public const string Suffix = ".dll";
+    #elif __APPLE__
+        public const string Prefix = "lib";
+        public const string Suffix = ".dylib";
+    #elif __linux__
+        public const string Prefix = "lib";
+        public const string Suffix = ".so";
+    #elif __FreeBSD__
+        public const string Prefix = "lib";
+        public const string Suffix = ".so";
+    #else
+        public const string Prefix = "";
+        public const string Suffix = "";
+    #endif
+
+        
+    #if _WIN32
+        public const string OS = "Windows";
+    #elif __APPLE__
+        public const string OS = "Apple";
+    #elif __linux__
+        public const string OS = "Linux";
+    #elif __FreeBSD__
+        public const string OS = "FreeBSD";
+    #else
+        public const string OS = "Unknown";
+    #endif
+
+    }
+
+    public class Console
     {
         private const string Dllname = @"" + DllHandle.Prefix + "cplusplus" + DllHandle.Suffix;
     }
