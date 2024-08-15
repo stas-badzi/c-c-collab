7b5b3509f8b6e239ee89942ab3e3d17de50e0db1
stas-badzi <sss.badziak@gmail.com>
2024-08-15 15:01:50 +0200

    fix naming

--- binarysharp/Program.cs
+++ binarysharp/Program.cs
@@ -6,7 +6,7 @@ namespace BinarySharp {
             List<string> file = new List<string>();
             file.Add("Badzi");
             FileSystem.ExportText("a.dat",file);
-            Filesystem.PlayMP3("boom.mp3");
+            FileSystem.PlayMP3("boom.mp3");
             return 0;
         }
     }
