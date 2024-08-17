464af3353807453d7856e7e7588ed45fb164b801
majkol333 <michal.kindziuk@gmail.com>
2024-08-17 17:59:34 +0200

    no changes (github won't let me work)

--- binarysharp/Program.cs
+++ binarysharp/Program.cs
@@ -3,10 +3,10 @@
 namespace BinarySharp {
     public class Program {
         static int Main(string[] args) {
-            List<string> file = new List<string>();
-            file.Add("Badzi");
-            FileSystem.ExportText("a.dat",file);
-            FileSystem.PlayMP3("boom.mp3");
+            //List<string> file = new List<string>();
+            //file.Add("Badzi");
+            //FileSystem.ExportText("a.dat",file);
+            FileSystem.PlayMP3(@"C:\Users\micha\Desktop\goat.mp3");
             return 0;
         }
     }
