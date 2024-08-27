e9d5405d043bfd4a50656aa403047fafe7e09eae
stas-badzi <sss.badziak@gmail.com>
2024-08-28 01:40:58 +0200

    fixed linux keyboard + more

--- binaryplus/src/main.cpp
+++ binaryplus/src/main.cpp
@@ -10,6 +10,10 @@
 #include <stdlib.h>
 #include <thread>
 
+#include <sys/ioctl.h>
+#include <linux/kd.h>
+#include <getfd.h>
+
 #ifdef _WIN32
     #define at_quick_exit(func) atexit(func)
     #define quick_exit(code) exit(code)
@@ -25,50 +29,26 @@ using namespace cs;
 using namespace std;
 
 int main() {
+    int mode1,mode2,fd,err;
+
     Console::Init();
+
     vector<vector<Console::Symbol>> scr;
-    
-//*
-    while (true) {
-        
-        /*
-        thread display([&] {
-            Console::Symbol sym;
-            sym.character(WCharToNative(L'▚'));
-            vector<vector<Console::Symbol>> list;
-            for (int l = 0; l < Console::GetWindowHeight(); l++) {
-                vector<Console::Symbol> row;
-                for (int i = 0; i < Console::GetWindowWidth(); i++) {
-                    row.push_back(sym);
-                    row.back().foreground(rand() % 16);
-                    row.back().background(rand() % 16);
-                }
-                list.push_back(row);
-            }
-            
-            
-            Console::FillScreen(list);
-        });
-        //*/
-        cerr << "a\n";
-            int out = Console::HandleKeyboard();
-            cerr << out << '\n';
-            const char * action = (out / 256) ? " up" : " down";
+    int run = true;
 
-            if (Console::KeyDown(125)) cerr << '^' << ':' << Console::KeyDown(29) << ' ' << 'q' << ':' << Console::KeyDown(16) << '\n';
-            if (Console::KeyDown(29) && Console::KeyHit(16)) {
-                int out = system(console_clear);
-                cerr << "Exiting";
-                cerr << '.';
-                cerr << '.';
-                cerr << '.';
-                exit(0);
-            }
-            if (out > 0) if (!Console::KeyDown(56)) cerr << out % 256 << action << '\n';
-        
-            int action_size = (out / 256) ? 3 : 5;
+    thread kbd_handle = thread([&](int* run) {
+            while (*run) {
+                int out = Console::HandleKeyboard();
+                const char * action = (out / 256) ? " up" : " down";
 
-        thread kbd_handle = thread([&] {
+                if (Console::KeyDown(125)) cerr << '^' << ':' << Console::KeyDown(29) << ' ' << 'q' << ':' << Console::KeyDown(16) << '\n';
+                if (out > 0) if (!Console::KeyDown(56)) cerr << out % 256 << action << '\n';
+            
+                int action_size = (out / 256) ? 3 : 5;
+            }
+            Console::Sleep(0.1);
+            cerr << "AAAAAAAAAAAAAAAA";
+            return;
             
             /*if (scr.size() >= Console::GetWindowHeight()) {
                 scr.erase(scr.begin());
@@ -85,13 +65,36 @@ int main() {
             }
             Console::FillScreen(scr);*/
             
+        },&run);
+//*
+    while (true) {
+        
+        //*
+        thread display([&] {
+            Console::Symbol sym;
+            sym.character(WCharToNative(L'▚'));
+            vector<vector<Console::Symbol>> list;
+            for (int l = 0; l < Console::GetWindowHeight(); l++) {
+                vector<Console::Symbol> row;
+                for (int i = 0; i < Console::GetWindowWidth(); i++) {
+                    row.push_back(sym);
+                    row.back().foreground(rand() % 16);
+                    row.back().background(rand() % 16);
+                }
+                list.push_back(row);
+            }
+            
+            Console::FillScreen(list);
         });
-        
-        
-        //display.join();
-        kbd_handle.join();
+
+        display.join();
+        //*/
 
         Console::Sleep(1.0);
+        
+        if (rand() % 30 == 0) {
+            run = false;
+        }
     }
 //*/
     return 0;
