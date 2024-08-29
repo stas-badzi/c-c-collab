6999862197110eda039b0b422923e0013dc9e6f9
stas-badzi <sss.badziak@gmail.com>
2024-08-29 17:14:53 +0200

    test

--- binarysharp/Program.cs
+++ binarysharp/Program.cs
@@ -8,22 +8,7 @@ using Utility;
 namespace BinarySharp {
     public class Program {
         static int Main(string[] args) {
-            Terminal.Init();
-            Console.WriteLine("aaaaaaaaaaaaaaaa");
-
-            /*
-            while (true) {
-                Terminal.HandleKeyboard();
-                Console.WriteLine("bbbbbb");
-                Console.WriteLine(Terminal.KeyHit(42));
-                Console.WriteLine("ccccccccccc");
-                Console.WriteLine(Terminal.KeyReleased(57));
-
-                if (false && Terminal.KeyHit(16) && Terminal.KeyDown(29)) {
-                    break;
-                }
-            }
-            */
+            Cs.FileSystem.PlayMP3("boom.mp3");
             return 0;
         }
     }
