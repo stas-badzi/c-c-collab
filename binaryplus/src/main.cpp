829aae7db451666e246b7fd56f1d4da02d22eeea
majkol333 <michal.kindziuk@gmail.com>
2024-09-16 18:07:55 +0200

    ezez

--- binaryplus/src/main.cpp
+++ binaryplus/src/main.cpp
@@ -11,13 +11,7 @@ using namespace cs;
 
 int main() {
     Console::Init();
-    while (!Console::IsKeyDown(VK_CONTROL) || Console::KeyPressed() != 'Q') {
-        Console::HandleMouseAndFocus();
-        wstring val;
-        val.append(L"Hello, World!" + to_wstring(Console::GetMouseStatus().y) + L" y: " + to_wstring(Console::GetMouseStatus().x) + L" abcdefg  h     ezzzzzzzzzzzzzzzzz");
-        auto p = Console::Symbol::CreateTexture(val);
-        Console::FillScreen(p);
-        Console::HandleKeyboard();
-    }
+    auto texture = FileSystem::TextureFromFile(L"C:\\Users\\micha\\Documents\\GitHub\\c-c-collab\\assets\\a.tux");
+    FileSystem::FileFromTexture(L"C:\\Users\\micha\\Documents\\GitHub\\c-c-collab\\assets\\b.tux", texture);
     return 0;
 }
