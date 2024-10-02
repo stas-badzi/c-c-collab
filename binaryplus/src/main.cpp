ae8c987069de40dad3a27081186ff683280bb32d
stas-badzi <sss.badziak@gmail.com>
2024-10-02 21:49:58 +0200

    czemu ustawiasz tlo i kolor tekxtu na ro samo????? + fixes

--- binaryplus/src/main.cpp
+++ binaryplus/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include <vector>
 
 #include "Console.hpp"
 #include "FileSystem.hpp"
@@ -17,15 +18,25 @@ int main() {
         auto width = Console::GetWindowWidth();
         auto height = Console::GetWindowHeight();
 
-        wstring characters[] = new wstring[width]();
-        uint8_t colors[] = new uint8_t[width]();
-        for (int16_t i = 0; i < width; i++) {
-            characters[i] = "#";
-            colors[i] = 7;
+        vector<vector<Console::Symbol>> scr;
+
+        wstring* characters = new wstring[(width+1)*height - 1]();
+        uint8_t* back = new uint8_t[width]();
+        uint8_t* fore = new uint8_t[width]();
+        //po new jest delete;
+        //ale jakby co masz vectory i vector.data() / vector.dat() nie pamietam
+        for (int16_t l = 0; l < height; l++) {
+            for (int16_t i = 0; i < width; i++) {
+                characters[i] = L'#';
+                back[i] = 7;
+                fore[i] = 2;
+            }
         }
-        auto screen = Console::Symbol::CreateTexture(characters, height, colors, colors);
+        auto screen = Console::Symbol::CreateTexture(characters, height, back, fore);
+        delete[] characters; delete[] back; delete[] fore;
+
         auto texture = FileSystem::TextureFromFile(System::ToNativePath(System::GetRootPath() + L"/assets/a.tux"));
-        FileSystem::DrawTextureToScreen(3,10,texture,screen);
+        FileSystem::DrawTextureToScreen(3,10,texture,screen); //to nie zadziala bo screen jest za maly;
         Console::FillScreen(screen);
         Control::CleanMemory();
         Console::HandleMouseAndFocus();
