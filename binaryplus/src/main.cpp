5545d57b2b636c2a8345a4699ff88f26256a029c
stas-badzi <sss.badziak@gmail.com>
2024-10-02 21:50:21 +0200

    fix1

--- binaryplus/src/main.cpp
+++ binaryplus/src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <array>
-#include <vector>
 
 #include "Console.hpp"
 #include "FileSystem.hpp"
@@ -18,19 +17,15 @@ int main() {
         auto width = Console::GetWindowWidth();
         auto height = Console::GetWindowHeight();
 
-        vector<vector<Console::Symbol>> scr;
-
-        wstring* characters = new wstring[(width+1)*height - 1]();
+        wstring* characters = new wstring[width]();
         uint8_t* back = new uint8_t[width]();
         uint8_t* fore = new uint8_t[width]();
         //po new jest delete;
         //ale jakby co masz vectory i vector.data() / vector.dat() nie pamietam
-        for (int16_t l = 0; l < height; l++) {
-            for (int16_t i = 0; i < width; i++) {
-                characters[i] = L'#';
-                back[i] = 7;
-                fore[i] = 2;
-            }
+        for (int16_t i = 0; i < width; i++) {
+            characters[i] = L'#';
+            back[i] = 7;
+            fore[i] = 2;
         }
         auto screen = Console::Symbol::CreateTexture(characters, height, back, fore);
         delete[] characters; delete[] back; delete[] fore;
