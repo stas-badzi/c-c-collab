0a3213d2f5dcd8a8e47810f356ddc9c545e8917b
majkol333 <michal.kindziuk@gmail.com>
2025-01-04 15:58:36 +0100

    Fixes in Cs Classes (part 3)
    fixes in binarysharp

--- binarysharp/FileSystem.cs
+++ binarysharp/FileSystem.cs
@@ -4,62 +4,9 @@ using Cpp;
 using System.Text.Json.Serialization.Metadata;
 
 namespace Cs {
-    public class FileSystem {
-        public static ulong Add(uint a, byte b) {
-            return CsImp.FileSystem.Add(a, b);
-        }
-
-        public static char DoSomething(bool tak, char c) {
-            return TypeConvert.UnicodeToUtf8(CsImp.FileSystem.DoSomething(tak, TypeConvert.Utf8ToUnicode(c)));
-        }
-
-        public static string DoSomeThings(List<bool> taki, string str) {
-            int byte_size = sizeof(byte);
-            int int32_size = sizeof(Int32);
-
-            nint input = Exec.AllocateMemory((nuint)(int32_size + (byte_size * taki.Count)));
-
-            Exec.WritePointer<Int32>(input, taki.Count);
-
-            for (int i = 0; i < taki.Count; i++) {
-                byte b = 0;
-                if (taki[i]) {b = 1;}
-                Exec.WritePointer<Byte>(input, int32_size + (i * byte_size), b);
-            }
-
-            return TypeConvert.PtrToString(CsImp.FileSystem.DoSomeThings(input, TypeConvert.StringToPtr(str)));
-        }
-
-        public static List<ulong> DoMoreThings(List<string> ls) {
-            int nint_size = nint.Size;
-            int int_size = sizeof(int);
-            int long_size = sizeof(long);
-
-            nint input = Exec.AllocateMemory((nuint)(int_size + (ls.Count * nint_size)));
-            Exec.WritePointer<Int32>(input, ls.Count);
-
-            for (int i = 0; i < ls.Count; i++) {
-                nint ptr = TypeConvert.StringToPtr(ls[i]);
-                Exec.WritePointer<IntPtr>(input, int_size + (i * nint_size), ptr);
-            }
-
-            nint ret = CsImp.FileSystem.DoMoreThings(input);
-
-            List<ulong> output = new List<ulong>();
-
-            Int32 size = Exec.ReadPointer<Int32>(ret);
-            for (int i = 0; i < size; i++) {
-                Int64 signed = Exec.ReadPointer<Int64>(ret, int_size + (i*long_size));
-                output.Add(Convert.ToUInt64(signed));
-            }
-
-            Exec.FreeMemory(ret);
-
-            return output;
-        }
-
+    public class TextureSystem {
         public static List<String> ImportText(String filestr) {
-            IntPtr ret = CsImp.FileSystem.ImportText(TypeConvert.StringToPtr(filestr));
+            IntPtr ret = CsImp.TextureSystem.ImportText(TypeConvert.StringToPtr(filestr));
             int intptr_size = IntPtr.Size;
             List<String> text = new List<String>();
 
@@ -86,13 +33,13 @@ namespace Cs {
             }
             IntPtr end = TypeConvert.StringToPtr("\u0000");
             Exec.WritePointer<IntPtr>(output, intptr_size * contentstr.Count, end);
-            CsImp.FileSystem.ExportText(TypeConvert.StringToPtr(pathstr), output);
+            CsImp.TextureSystem.ExportText(TypeConvert.StringToPtr(pathstr), output);
         }
 
         public static List<List<Terminal.Symbol>> TextureFromFile(string filepath)
         {
             nint filepathPtr = TypeConvert.StringToPtr(filepath);  // Convert filepath back
-            nint func = CsImp.FileSystem.TextureFromFile(filepathPtr); // Get exported function
+            nint func = CsImp.TextureSystem.TextureFromFile(filepathPtr); // Get exported function
 
             var texture = TypeConvert.PtrToTexture(func);
 
@@ -104,7 +51,7 @@ namespace Cs {
             nint filepathPtr = TypeConvert.StringToPtr(filepath);
             nint texturePtr = TypeConvert.TextureToPtr(texture);
 
-            CsImp.FileSystem.FileFromTexture(filepathPtr, texturePtr, recycle);
+            CsImp.TextureSystem.FileFromTexture(filepathPtr, texturePtr, recycle);
             Exec.FreeMemory(texturePtr);
         }
         public static void DrawTextureToScreen(int x, int y, List<List<Terminal.Symbol>> texture, List<List<Terminal.Symbol>> screen)
@@ -112,13 +59,13 @@ namespace Cs {
             nint texturePtr = TypeConvert.TextureToPtr(texture);
             nint screenPtr = TypeConvert.TextureToPtr(screen);
 
-            CsImp.FileSystem.DrawTextureToScreen(x, y, texturePtr, screenPtr);
+            CsImp.TextureSystem.DrawTextureToScreen(x, y, texturePtr, screenPtr);
             Exec.FreeMemory(texturePtr);
             Exec.FreeMemory(screenPtr);
         }
         public static void PlaySound(string filepath, bool wait = false)
         {
-            CsImp.FileSystem.PlaySound(TypeConvert.StringToPtr(filepath),wait);
+            CsImp.TextureSystem.PlaySound(TypeConvert.StringToPtr(filepath),wait);
         }
     }
 }
--- binarysharp/Program.cs
+++ binarysharp/Program.cs
@@ -6,7 +6,7 @@ using Utility;
 namespace BinarySharp {
     public class Program {
         static int Main(string[] args) {
-            FileSystem.PlaySound(Path.Combine([AppContext.BaseDirectory,"..","sounds","shoot.wav"]), true);
+            TextureSystem.PlaySound(Path.Combine([AppContext.BaseDirectory,"..","sounds","shoot.wav"]), true);
             return 0;
         }
     }
