e949ecd5dccb5c01e23ff8fb61534dfbb0e3a6ce
majkol333 <michal.kindziuk@gmail.com>
2025-01-22 19:20:43 +0100

    s

--- cplusplus/src/TextureSystem.hpp
+++ cplusplus/src/TextureSystem.hpp
@@ -20,8 +20,8 @@ namespace cs {
         static void ExportText(std::wstring file, std::vector<std::wstring> lines);
         static std::vector<std::vector<cpp::Console::Symbol> > TextureFromFile(std::wstring filepath);
         static void FileFromTexture(std::wstring filepath, std::vector<std::vector<cpp::Console::Symbol> > texture, bool recycle = false);
-        static void DrawTextureToScreen(int x, int y, std::vector<std::vector<cpp::Console::Symbol> > texture, std::vector<std::vector<cpp::Console::Symbol> > screen);s
+        static void DrawTextureToScreen(int x, int y, std::vector<std::vector<cpp::Console::Symbol> > texture, std::vector<std::vector<cpp::Console::Symbol> > screen);
     };
 }
-s
+
 #include "TextureSystem.ipp"
