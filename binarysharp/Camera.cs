62cbbc0fb984859423b84dfbed0cd340e091f88d
majkol333 <michal.kindziuk@gmail.com>
2025-02-05 12:56:07 +0100

    sekundy przed zanikiem pamieci

--- binarysharp/Camera.cs
+++ binarysharp/Camera.cs
@@ -1,5 +1,18 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using Microsoft.VisualBasic.FileIO;
+using Cs;
+using Cpp;
+using Texture = System.Collections.Generic.List<System.Collections.Generic.List<Cpp.Terminal.Symbol>>;
+
 namespace Cs {
     public class Camera {
-        
+        public Texture buffer { get; }
+        public Tuple<int, int> viewportCenter { get; }
+
+        public void DrawTextureToCamera(Texture texture, Tuple<int, int> center) {
+            
+        }
     }
 }
