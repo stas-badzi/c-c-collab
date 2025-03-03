611e0a3e851dc0e3356ac7304bde54339b82d42e
stas-badzi <stasbadzi@staszic.waw.pl>
2025-03-03 22:43:26 +0100

    tsst

--- utilities/custom/vsdevcmd-x64.bat
+++ utilities/custom/vsdevcmd-x64.bat
@@ -1,1 +1,3 @@
+ls "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+%*
