b7648e3a02ea2ac59d955116fefb20df90375e31
stas-badzi <stasbadzi@staszic.waw.pl>
2025-03-03 23:27:27 +0100

    winfix10

--- utilities/custom/vsdevcmd-x64.bat
+++ utilities/custom/vsdevcmd-x64.bat
@@ -1,20 +1,2 @@
-ls "C:"
-echo _
-ls "C:\Program Files"
-echo _
-ls "C:\Program Files\Microsoft Visual Studio"
-echo _
-ls "C:\Program Files\Microsoft Visual Studio\2022"
-echo _
-ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise"
-echo _
-ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC"
-echo _
-ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary"
-echo _
-ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build"
-echo _
-ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
-echo _
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 %*
