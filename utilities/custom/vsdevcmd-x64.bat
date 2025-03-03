51753fbccc609369f92aa62ea71732f0a9bbf534
stas-badzi <stasbadzi@staszic.waw.pl>
2025-03-03 23:16:32 +0100

    fixnow

--- utilities/custom/vsdevcmd-x64.bat
+++ utilities/custom/vsdevcmd-x64.bat
@@ -1,20 +1,20 @@
 ls "C:"
-echo
-ls "C:\Program Files (x86)"
-echo
-ls "C:\Program Files (x86)\Microsoft Visual Studio"
-echo
-ls "C:\Program Files (x86)\Microsoft Visual Studio\2022"
-echo
-ls "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise"
-echo
-ls "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC"
-echo
-ls "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary"
-echo
-ls "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build"
-echo
-ls "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
-echo
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+echo _
+ls "C:\Program Files"
+echo _
+ls "C:\Program Files\Microsoft Visual Studio"
+echo _
+ls "C:\Program Files\Microsoft Visual Studio\2022"
+echo _
+ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise"
+echo _
+ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC"
+echo _
+ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary"
+echo _
+ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build"
+echo _
+ls "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
+echo _
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 %*
