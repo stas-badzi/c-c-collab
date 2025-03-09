2770cf7391c6a39c7f4c50b667c0d23c340bfd43
majkol333 <michal.kindziuk@gmail.com>
2025-03-09 14:48:26 +0100

    smart_ref implementation

--- cplusplus/src/Game.cpp
+++ cplusplus/src/Game.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <algorithm>
 #include "Console.hpp"
+#include "smart_ref.hpp"
 
 #ifdef max
 #undef max
@@ -19,67 +20,16 @@ using std::vector;
 // public:
 
 Game::Camera::Camera(int height, int width, Console::Symbol sym) {
-	this->buffer = vector<vector<Console::Symbol>>(height, vector<Console::Symbol>(width, sym));
+	this->buffer = vector<vector<smart_ref<Console::Symbol>>>(height, vector<smart_ref<Console::Symbol>>(width, smart_ref(sym)));
+
+    ValidateViewport(this->ViewportCenter());
 }
 
-Game::Camera::Camera(void* cameraPtr) {
-    int offset = 0;
-    const size_t int32_size = sizeof(int);
-    const size_t symptr_size = sizeof(Console::Symbol*);
-
-    vector<vector<Console::Symbol>> ret_buffer;
-
-    int height = *((int*)cameraPtr+offset);
-    offset += int32_size;
-
-    buffer.resize(height);
-
-    for (int i = 0; i < height; i++) {
-        int width = *((int*)cameraPtr+offset);
-        offset += int32_size;
-
-        buffer[i].resize(width);
-        for (int j = 0; j < width; j++) {
-            buffer[i][j] = *((Console::Symbol*)cameraPtr+offset);
-            offset += symptr_size;
-        }
-    }
-
-    this->buffer = ret_buffer;
-
-    free(cameraPtr);
+Game::MatrixPosition Game::Camera::ViewportCenter() {
+    return MatrixPosition(floor(this->buffer.size()/2), floor(this->buffer[0].size()/2));
 }
 
-void* Game::Camera::Get() {
-	int alloc = 0;
-    const size_t int32_size = sizeof(int);
-    const size_t symptr_size = sizeof(Console::Symbol*);
-
-    alloc += int32_size; // buffer.Count
-    for (size_t i = 0; i < this->buffer.size(); i++) {
-        alloc += int32_size + buffer[i].size() * symptr_size; // buffer[i].size() + buffer[i]
-    }
-
-    int offset = 0;
-    void* ret = malloc(alloc);
-
-    *((int*)ret+offset) = this->buffer.size();
-    offset += int32_size;
-    
-    for (size_t i = 0; i < this->buffer.size(); i++) {
-        auto row = buffer[i];
-        *((int*)ret+offset) = row.size();
-        offset += int32_size;
-        for (size_t j = 0; j < row.size(); j++) {
-            *((Console::Symbol*)ret+offset) = row[j];
-            offset += symptr_size;
-        }
-    }
-
-    return ret;
-}
-
-void Game::Camera::DrawTexture(vector<vector<Console::Symbol>> texture, MatrixPosition center) {
+void Game::Camera::DrawTexture(vector<vector<smart_ref<Console::Symbol>>> texture, MatrixPosition center) {
     float height = (float)texture.size();
     float width = (float)texture[0].size();
 
@@ -92,12 +42,13 @@ void Game::Camera::DrawTexture(vector<vector<Console::Symbol>> texture, MatrixPo
 
     for (int i = startX, ti = 0; i < endX; i++, ti++) {
         for (int j = startY, tj = 0; j < endY; j++, tj++) {
-            this->buffer[i][j] = texture[ti][tj];
+            this->buffer[i][j] = smart_ref(texture[ti][tj]());
         }
     }
 }
 
 // private:
+
 void Game::ValidateViewport(MatrixPosition vpc) {
     if (vpc.iIndex % 2 == 0 || vpc.jIndex % 2 == 0) {
 		throw std::invalid_argument("Width and height must be odd numbers.");
--- cplusplus/src/Game.hpp
+++ cplusplus/src/Game.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "Console.hpp"
+#include "smart_ref.hpp"
 
 namespace cpp {
     class Game {
@@ -12,12 +13,10 @@ namespace cpp {
             struct Camera {
                 public:
                     Camera(int height, int width, cpp::Console::Symbol sym);
-                    Camera(void* cameraPtr); // don't know if it is useful at all
-                    void* Get(); //
-                    void DrawTexture(std::vector<std::vector<Console::Symbol>> texture, MatrixPosition center);
+                    MatrixPosition ViewportCenter();
+                    void DrawTexture(std::vector<std::vector<smart_ref<cpp::Console::Symbol>>> texture, MatrixPosition center);
                 private:
-                    std::vector<std::vector<cpp::Console::Symbol>> buffer;
-                    //Camera(std::vector<std::vector<Console::Symbol>> buffer, MatrixPosition viewportCenter);
+                    std::vector<std::vector<smart_ref<cpp::Console::Symbol>>> buffer;
             };
         private:
             static void ValidateViewport(MatrixPosition vpc);
