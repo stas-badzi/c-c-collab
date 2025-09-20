1ef5f5092fa82d23f127ca5acd23642efd738141
stas-badzi <stasbadzi@staszic.waw.pl>
2025-09-20 13:27:25 +0200

    fix nullptr not existent in obj-c

--- source/openfile.m
+++ source/openfile.m
@@ -6,8 +6,8 @@ int openFileInApp(const char* filepath, const char* app) {
     CFURLRef url = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, path, kCFURLPOSIXPathStyle, false);
     CFURLRef fileURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, filePath, kCFURLPOSIXPathStyle, false);
     CFArrayRef array = CFArrayCreate(kCFAllocatorDefault, (const void **)&fileURL, 1, NULL);
-    LSLaunchURLSpec spec = { url,array,NULL, kLSLaunchAsync | kLSLaunchNewInstance, nullptr };
-    OSStatus err = LSOpenFromURLSpec(&spec,nullptr);
+    LSLaunchURLSpec spec = { url,array,NULL, kLSLaunchAsync | kLSLaunchNewInstance, NULL };
+    OSStatus err = LSOpenFromURLSpec(&spec,NULL);
     return err;
 }
 
@@ -15,7 +15,7 @@ int openFile(const char* filepath) {
     CFStringRef filePath = CFStringCreateWithCString (NULL, filepath, kCFStringEncodingUTF8);
     CFURLRef fileURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, filePath, kCFURLPOSIXPathStyle, false);
     CFArrayRef array = CFArrayCreate(kCFAllocatorDefault, (const void **)&fileURL, 1, NULL);
-    LSLaunchURLSpec spec = { NULL,array,NULL, kLSLaunchAsync | kLSLaunchNewInstance, nullptr };
-    OSStatus err = LSOpenFromURLSpec(&spec,nullptr);
+    LSLaunchURLSpec spec = { NULL,array,NULL, kLSLaunchAsync | kLSLaunchNewInstance, NULL };
+    OSStatus err = LSOpenFromURLSpec(&spec,NULL);
     return err;
 }
