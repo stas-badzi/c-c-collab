267acc7c925ec55630386819debfd939e040577b
stas-badzi <sss.badziak@gmail.com>
2026-05-02 20:41:12 +0100

    add correct pointer casting

--- cplusplus/include/utfchar.h
+++ cplusplus/include/utfchar.h
@@ -8,6 +8,8 @@ typedef unsigned int char32_t;
 #endif
 size_t mbrtoc32 (char32_t *__restrict__ __pc32, const char *__restrict__ __s, size_t __n, mbstate_t *__restrict__ __p);
 size_t c32rtomb(char *__restrict__ __s, char32_t __c32, mbstate_t *__restrict__ __ps);
-static size_t mbrtoc16(char16_t *__restrict__ __pc16, const char *__restrict__ __s, size_t __n, mbstate_t *__restrict__ __p) {return mbrtowc(__pc16, __s, __n, __p);}
+#ifdef __CYGWIN32__
+static size_t mbrtoc16(char16_t *__restrict__ __pc16, const char *__restrict__ __s, size_t __n, mbstate_t *__restrict__ __p) {return mbrtowc((wchar_t*)__pc16, __s, __n, __p);}
 static size_t c16rtomb(char *__restrict__ __s, char16_t __c16, mbstate_t *__restrict__ __ps) {return wcrtimb(__s, __c16, __ps);}
+#endif
 #endif
