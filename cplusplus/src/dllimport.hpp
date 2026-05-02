b4941f90bb3a40ac8604b4e72b07fcfba19bf454
stas-badzi <sss.badziak@gmail.com>
2026-05-02 16:09:20 +0100

    remove rest of custom sanitizer parts

--- cplusplus/src/dllimport.hpp
+++ cplusplus/src/dllimport.hpp
@@ -12,103 +12,60 @@ namespace cppimp {
     // Symbol
         inline void* Console_Symbol_Construct$smb(cpp::Console::Symbol* src) {
             void* out = (void*) new cpp::Console::Symbol(*src);
-        #ifdef _DEBUG
-            __save$SYMBOLS(out);
-        #endif
             return out;
         }
 
 
         inline void* Console_Symbol_Construct$cfb(uniconv::unichar character, uint8_t foreground = 7, uint8_t background = 0) {
             void* out = new cpp::Console::Symbol(uniconv::UnicodeToNative(character), foreground, background);
-        #ifdef _DEBUG
-            __save$SYMBOLS(out);
-        #endif
             return out;
         }
 
         #ifdef _WIN32
             inline void* Console_Symbol_Construct$atr(uint8_t attribute) {
                 void* out = (void*) new cpp::Console::Symbol(attribute);
-            #ifdef _DEBUG
-                __save$SYMBOLS(out);
-            #endif
                 return out;
             }
             
             inline void Console_Symbol_SetAttribute(cpp::Console::Symbol* smb, uint8_t attribute) {
-            #ifdef _DEBUG
-                __check$SYMBOLS(smb);
-            #endif
                 smb->SetAttribute(attribute);
             }
 
             inline uint8_t Console_Symbol_GetAttribute(cpp::Console::Symbol* smb) {
-            #ifdef _DEBUG
-                __check$SYMBOLS(smb);
-            #endif
                 return smb->GetAttribute();
             }
         #endif
 
         inline void Console_Symbol_character$set(cpp::Console::Symbol* smb, uniconv::unichar character) {
-        #ifdef _DEBUG
-            __check$SYMBOLS(smb);
-        #endif
-            smb->character = uniconv::UnicodeToNative(character);
+        smb->character = uniconv::UnicodeToNative(character);
         }
 
         inline uniconv::unichar Console_Symbol_character$get(cpp::Console::Symbol* smb) {
-        #ifdef _DEBUG
-            __check$SYMBOLS(smb);
-        #endif
-            return uniconv::NativeToUnicode(smb->character);
+        return uniconv::NativeToUnicode(smb->character);
         }
 
         inline void Console_Symbol_foreground$set(cpp::Console::Symbol* smb, uint8_t foreground) {
-        #ifdef _DEBUG
-            __check$SYMBOLS(smb);
-        #endif
-            smb->foreground = foreground;
+        smb->foreground = foreground;
         }
 
         inline uint8_t Console_Symbol_foreground$get(cpp::Console::Symbol* smb) {
-        #ifdef _DEBUG
-            __check$SYMBOLS(smb);
-        #endif
-            return smb->foreground;
+        return smb->foreground;
         }
 
         inline void Console_Symbol_background$set(cpp::Console::Symbol* smb, uint8_t background) {
-        #ifdef _DEBUG
-            __check$SYMBOLS(smb);
-        #endif
-            smb->background = background;
+        smb->background = background;
         }
 
         inline uint8_t Console_Symbol_background$get(cpp::Console::Symbol* smb) {
-        #ifdef _DEBUG
-            __check$SYMBOLS(smb);
-        #endif
-            return smb->background;
+        return smb->background;
         }
 
         inline void Console_Symbol_Destruct(cpp::Console::Symbol* smb) {
-        #ifdef _DEBUG
-            __free$SYMBOLS(smb);
-        #endif
             delete smb;
         }
 
         inline void* Console_Symbol_operator$eq(cpp::Console::Symbol* cp, cpp::Console::Symbol* src) {
-        #ifdef _DEBUG
-            __check$SYMBOLS(src);
-            __check$SYMBOLS(cp);
-        #endif
             void* out = (void*) new cpp::Console::Symbol( (*cp) = (*src) );
-        #ifdef _DEBUG
-            __save$SYMBOLS(out);
-        #endif
             return out;
         }
     // ~Symbol
