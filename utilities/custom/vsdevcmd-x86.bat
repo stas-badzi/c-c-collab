9bd32636e4fb5486556966345840e9ad3a4e1b28
stas-badzi <sss.badziak@gmail.com>
2026-09-20 13:37:20 +0200

    update visual studio vesion

--- utilities/custom/vsdevcmd-x86.bat
+++ utilities/custom/vsdevcmd-x86.bat
@@ -1,2 +1,2 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
+call "C:\Program Files\Microsoft Visual Studio\2026\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
 %*
