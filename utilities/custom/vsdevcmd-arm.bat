00453f8be10cd0301e818f2d00f30ed38a4a6483
stas-badzi <sss.badziak@gmail.com>
2026-09-20 13:06:58 +0200

    debug info

--- utilities/custom/vsdevcmd-arm.bat
+++ utilities/custom/vsdevcmd-arm.bat
@@ -1,2 +1,15 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm 
+
+where clang-cl
+where link
+
+echo WindowsSdkDir=%WindowsSdkDir%
+echo LIB=%LIB%
+
+dir "%WindowsSdkDir%Lib\*\um\arm\USER32.lib"
+if errorlevel 1 (
+    echo ARM32 USER32.lib was not found
+    exit /b 1
+)
+
 %*
