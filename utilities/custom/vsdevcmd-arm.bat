d84ba51c953b9c4e977bce03b6e3f494bc5b06e8
stas-badzi <sss.badziak@gmail.com>
2026-09-20 13:35:19 +0200

    try fix arm32

--- utilities/custom/vsdevcmd-arm.bat
+++ utilities/custom/vsdevcmd-arm.bat
@@ -1,4 +1,6 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm 
+for /f "tokens=6 delims=\" %%V in ('dir /s /b "C:\Program Files (x86)\Windows Kits\10\Lib\arm" ^| findstr "\\um\\arm$"') do set "VERSION=%%V"
+echo %VERSION%
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm %VERSION%
 
 where clang-cl
 where link
