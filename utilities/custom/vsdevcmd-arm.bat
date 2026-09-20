bef679a6e1ff970a9b2c6cfa627c15d1c363f1bf
stas-badzi <sss.badziak@gmail.com>
2026-09-20 12:59:02 +0200

    tf microsoft?

--- utilities/custom/vsdevcmd-arm.bat
+++ utilities/custom/vsdevcmd-arm.bat
@@ -1,2 +1,2 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" arm
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm 
 %*
